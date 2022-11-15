6a24ca6402c7c9224f5cd69f6766910eefa92e0e
SAJID LATIF <indus1530@gmail.com>
2022-11-15 15:12:06 +0500

    initialCommit

---
+++ app/src/main/java/edu/aku/abdulsajid/nanm2022/ui/sections/SectionAActivity.java
@@ -0,0 +1,119 @@
+package edu.aku.abdulsajid.nanm2022.ui.sections;
+
+import static edu.aku.abdulsajid.nanm2022.core.MainApp.form;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.json.JSONException;
+
+import edu.aku.abdulsajid.nanm2022.R;
+import edu.aku.abdulsajid.nanm2022.contracts.TableContracts;
+import edu.aku.abdulsajid.nanm2022.core.MainApp;
+import edu.aku.abdulsajid.nanm2022.database.DatabaseHelper;
+import edu.aku.abdulsajid.nanm2022.databinding.ActivitySectionABinding;
+
+public class SectionAActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "SectionAActivity";
+    ActivitySectionABinding bi;
+    private DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(MainApp.langRTL ? R.style.AppThemeSindhi : R.style.AppThemeEnglish1);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_a);
+        setSupportActionBar(bi.toolbar);
+        db = MainApp.appInfo.dbHelper;
+        form.setA103(MainApp.currentHousehold.getSno());
+        bi.setForm(form);
+    }
+
+
+    private boolean insertNewRecord() {
+        if (!MainApp.form.getUid().equals("") || MainApp.superuser) return true;
+        MainApp.form.populateMeta();
+        long rowId = 0;
+        try {
+            rowId = db.addForm(MainApp.form);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this, R.string.db_excp_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        MainApp.form.setId(String.valueOf(rowId));
+        if (rowId > 0) {
+            MainApp.form.setUid(MainApp.form.getDeviceId() + MainApp.form.getId());
+            db.updatesFormColumn(TableContracts.FormsTable.COLUMN_UID, MainApp.form.getUid());
+            return true;
+        } else {
+            Toast.makeText(this, R.string.upd_db_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    private boolean updateDB() {
+        if (MainApp.superuser) return true;
+
+        int updcount = 0;
+        try {
+            updcount = db.updatesFormColumn(TableContracts.FormsTable.COLUMN_SA, MainApp.form.sAtoString());
+        } catch (JSONException e) {
+            Toast.makeText(this, R.string.upd_db + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, R.string.upd_db_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    public void btnContinue(View view) {
+        bi.llbtn.setVisibility(View.GONE);
+        new Handler().postDelayed(() -> bi.llbtn.setVisibility(View.VISIBLE), 5000);
+        if (!formValidation()) return;
+        if (!insertNewRecord()) return;
+        if (updateDB()) {
+            Intent i;
+            i = new Intent(this, ConsentActivity.class).putExtra("complete", true);
+            startActivity(i);
+            finish();
+        } else {
+            Toast.makeText(this, R.string.fail_db_upd, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void btnEnd(View view) {
+        finish();
+    }
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MainApp.lockScreen(this);
+    }
+
+
+}
---
+++ app/src/main/java/edu/aku/abdulsajid/nanm2022/ui/sections/SectionH1BActivity.java
@@ -0,0 +1,100 @@
+package edu.aku.abdulsajid.nanm2022.ui.sections;
+
+import static edu.aku.abdulsajid.nanm2022.core.MainApp.familyList;
+import static edu.aku.abdulsajid.nanm2022.core.MainApp.selectedMWRA;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.json.JSONException;
+
+import edu.aku.abdulsajid.nanm2022.R;
+import edu.aku.abdulsajid.nanm2022.contracts.TableContracts;
+import edu.aku.abdulsajid.nanm2022.core.MainApp;
+import edu.aku.abdulsajid.nanm2022.database.DatabaseHelper;
+import edu.aku.abdulsajid.nanm2022.databinding.ActivitySectionH1bBinding;
+import edu.aku.abdulsajid.nanm2022.ui.EndingActivity;
+
+
+public class SectionH1BActivity extends AppCompatActivity {
+
+    private static final String TAG = "SectionH1BActivity";
+    ActivitySectionH1bBinding bi;
+    int childAge;
+    private DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(MainApp.langRTL ? R.style.AppThemeSindhi : R.style.AppThemeEnglish1);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_h1b);
+        bi.setMwra(MainApp.mwra);
+        setSupportActionBar(bi.toolbar);
+        db = MainApp.appInfo.dbHelper;
+        bi.toolbar.setSubtitle("MWRA Child:\t\t" + "Sno: " + familyList.get(Integer.parseInt(selectedMWRA) - 1).getD101()
+                + ",\tInd: " + familyList.get(Integer.parseInt(selectedMWRA) - 1).getIndexed()
+                + ",\t Name: " + familyList.get(Integer.parseInt(selectedMWRA) - 1).getD102()
+                + " \t");
+    }
+
+
+    private boolean updateDB() {
+        if (MainApp.superuser) return true;
+        int updcount = 0;
+        try {
+            updcount = db.updatesMWRAColumn(TableContracts.MwraTable.COLUMN_SH1B, MainApp.mwra.sH1BtoString());
+        } catch (JSONException e) {
+            Toast.makeText(this, R.string.upd_db + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, R.string.upd_db_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+
+    }
+
+    public void btnContinue(View view) {
+        bi.llbtn.setVisibility(View.GONE);
+        new Handler().postDelayed(() -> bi.llbtn.setVisibility(View.VISIBLE), 5000);
+        if (!formValidation()) return;
+        if (updateDB()) {
+            finish();
+            startActivity(new Intent(this, SectionH2Activity.class).putExtra("complete", true));
+        } else {
+            Toast.makeText(this, R.string.fail_db_upd, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void btnEnd(View view) {
+        finish();
+        startActivity(new Intent(this, EndingActivity.class).putExtra("complete", false));
+    }
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MainApp.lockScreen(this);
+    }
+}
